786a416a328966fa1dfc6748f9696606859a5255
eduardo diaz <ediazshaw@gmail.com>
2022-05-31 10:14:15 +0200

    rutes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :entries, only: [:create, :index, :new, :show]
+  get "entries/search", to: "entries#search", as: :search
 end
