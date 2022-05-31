cbbf02a5c0bb7da3d243390d85037f6eedab1479
eduardo diaz <ediazshaw@gmail.com>
2022-05-31 11:47:25 +0200

    validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  validates :theme, presence: true
+  validates :remember_date, presence: true
+  validates :comment, length: { maximum: 10000, too_long: `%{count} characters is the maximum allowed` }
 end
