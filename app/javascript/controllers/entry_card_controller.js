3f14580483e471196918746331c9a5ba7cfafd56
Marcos Lutterbach <mlutterb@gmail.com>
2022-06-01 16:27:26 +0200

    completed the cards for new and added javascript to make them appear when the right radio button was toggled

---
+++ app/javascript/controllers/entry_card_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["card", "input"]
+
+  connect() {
+    console.log(this.contentTarget)
+  }
+
+  toggleCard(event) {
+    console.log(event.currentTarget.value)
+    const selected_value = event.currentTarget.value;
+    this.cardTargets.forEach(card => {
+      if (card.dataset.id == selected_value) {
+        card.classList.remove("d-none");
+      } else {
+        card.classList.add("d-none");
+      }
+    });
+  }
+}
