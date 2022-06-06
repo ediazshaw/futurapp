05804ccf3f9c6cb744382ee7a8fd258da970f07b
Joana Mena <joanamacarenamg@gmail.com>
2022-06-06 17:26:31 +0200

    Pushing the mailers notifications for welcome emails and for reminders

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,16 @@ class UserMailer < ApplicationMailer
   def entry_notification(entry_id)
     @entry = Entry.find(entry_id)
     mail(
-      to:       @entry.user.email,
-      subject:  "You have a memory to visit"
+      to: @entry.user.email,
+      subject: "You have a memory to visit"
+    )
+  end
+  
+  def welcome
+    @user = params[:user]
+    mail(
+      to: @user.email,
+      subject: 'Welcome to Futurapp'
     )
   end
 end
