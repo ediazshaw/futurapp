c36d4c905c1ef3a4625145177d7cafb8d954fcb8
Marcos Lutterbach <mlutterb@gmail.com>
2022-06-09 12:24:27 +0200

    push with date for today

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @question_day = QuestionDay.find_by(date: (Date.tomorrow.beginning_of_day..Date.tomorrow.end_of_day))
+    @question_day = QuestionDay.find_by(date: (Date.today.beginning_of_day..Date.today.end_of_day))
   end
 end
